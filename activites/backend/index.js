a9967c83ea3c4f408966c04f076a8faec48512b7
Diggeh <166510285+Diggeh@users.noreply.github.com>
2026-01-14 13:28:10 +0800

    week 2 hardcoded get post logic for login

--- activites/backend/index.js
+++ activites/backend/index.js
@@ -3,6 +3,31 @@ import experess from "express";
 const app = experess();
 const PORT = 3000;
 
+// get -> display name, var name = "Your Name"
+// post -> logic, if username="admin" and password="admin" -> success else failed
+
+app.use(experess.json());
+
+app.get("/getName", (req, res) => {
+  var name = "Your Name";
+  res.status(200).json(name);
+});
+
+app.post("/login", (req, res) => {
+  var { username, password } = req.body;
+  if (username === "admin" && password === "admin") {
+    res.status(200).json({
+      message: "Login Successfully.",
+      status: "success",
+    });
+  } else {
+    res.status(403).json({
+      message: "Invalid username or password.",
+      status: "failed",
+    });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
 });
